7da4a30a31f54b39042477b254479eb6f356a657
trueberryless <trueberryless+github@gmail.com>
2025-01-15 12:22:57 +0100

    prepare last quotes tests

---
+++ packages/starlight-spell-checker/tests/quotes.throwError.multiNested.test.ts
@@ -0,0 +1,29 @@
+import { expect, test } from 'vitest'
+
+import { ValidationErrorType } from '../libs/validation'
+
+import { buildFixture, expectValidationErrorCount, expectValidationErrors, expectValidationSuccess } from './utils'
+
+test('builds with quotes throw error multi nested valid English content', async () => {
+  const { output, status } = await buildFixture('quotes-throw-error-multi-nested-valid-content')
+
+  expect(status).toBe('success')
+  expectValidationSuccess(output)
+})
+
+test('builds with quotes throw error multi multi nested valid English content', async () => {
+  const { output, status } = await buildFixture('quotes-throw-error-multi-multi-nested-valid-content')
+
+  expect(status).toBe('success')
+  expectValidationSuccess(output)
+})
+
+test('does not build with quotes throw error multi nested invalid English content', async () => {
+  const { output, status } = await buildFixture('quotes-throw-error-multi-nested-invalid-content')
+
+  expect(status).toBe('error')
+
+  expectValidationErrorCount(output, , 1)
+
+  
+})
